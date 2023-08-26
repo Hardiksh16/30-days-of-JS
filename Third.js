1ffddf664b14968ecc1b667f40ab52672d804dda
Hardik Shetty <shettyshgermany@gmail.com>
2023-08-26 15:45:40 +0200

    Third js added

---
+++ Third.js
@@ -0,0 +1,8 @@
+// Program to factorize
+ let Factorize = 1;
+ let a = prompt("Enter the number to factorize");
+ a=Number.parseInt(a);
+ for(i=0;i<=a;i++);{
+Factorize( i*=i);
+ }
+console.log(Factorize);
