18e98c6dcb896ac7f1f349093b8165c20527a7ae
Hardik Shetty <shettyshgermany@gmail.com>
2023-08-27 11:45:56 +0200

    Factorization Code using for Loop

--- Third.js
+++ Third.js
@@ -1,8 +1,9 @@
 // Program to factorize
+
  let Factorize = 1;
  let a = prompt("Enter the number to factorize");
  a=Number.parseInt(a);
- for(i=0;i<=a;i++);{
-Factorize( i*=i);
+ for(let i=1; i<=a; i++) {
+ Factorize *= i;
  }
-console.log(Factorize);
+ console.log("The factorial of " + a + " is " + Factorize);
